df817bc797727f14e7cdcb21b161f1b41252cbdb
MANISH HOIYANI <hoiyanimanish@gmail.com>
2021-06-08 15:52:04 +0200

    Feature complete: Dictionary check of attempt

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
 # game.py
 # pylint: disable=missing-docstring
-#pylint: disable=too-few-public-methods
 
 import string
 import random
@@ -10,3 +9,15 @@ class Game:
         self.grid = []
         for _ in range(9):
             self.grid.append(random.choice(string.ascii_uppercase))
+import requests
+
+class Game:
+   def is_valid(self, word):
+        return self.__check_dictionary(word)
+
+
+@staticmethod
+  def __check_dictionary(word):
+        response = requests.get(f"https://wagon-dictionary.herokuapp.com/{word}")
+        json_response = response.json()
+        return json_response['found']
